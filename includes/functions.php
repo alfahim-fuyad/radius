ae6b6344476a29fd2944507da16526ed59306066
Al Fahim Fuad <md.alfahim.fuyad@gmail.com>
2026-08-15 12:38:36 +0600

    fix cookie

--- includes/functions.php
+++ includes/functions.php
@@ -5,9 +5,17 @@ declare(strict_types=1);
 |--------------------------------------------------------------------------
 | Start PHP Session
 |--------------------------------------------------------------------------
-
 */
+
 if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 60 * 60 * 24 * 30, // 30 days
+        'path' => '/',
+        'secure' => true,                // Render HTTPS
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+
     session_start();
 }
 
